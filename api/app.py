3df77206def5f6f95595acdec736da30d5c74d23
aveti_to <avetichamp@gmail.com>
2020-04-30 12:47:17 +0530

    Flask Api

--- api/app.py
+++ api/app.py
@@ -6,6 +6,6 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>Docker Python Tutorial</h1><p>Learnpython from AvetiTutorials</p>"
 
 app.run(host= '0.0.0.0')
